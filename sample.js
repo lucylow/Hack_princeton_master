a624143b63c1200a67929fa366c8b1e292cf1387
^˵◕ω◕˵^ <low.lucyy@gmail.com>
2019-08-17 10:36:53 -0400

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,12 +1,7 @@
-/*
- * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
 (function() {
     "use strict";
     
-    /* Recorder Stuff */
+    /* Recorder */
     var audio_context;
     var recorder;
 
